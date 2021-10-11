7428e3a493b62c39e28d077a3bab2b7592172140
John Pope <75339810+john-pope@users.noreply.github.com>
2021-10-11 17:11:12 -0600

    wip...

--- AIPImmunizationSurvey/ActionItemContactDetail_onload.js
+++ AIPImmunizationSurvey/ActionItemContactDetail_onload.js
@@ -1,69 +1,60 @@
-var contextSelector = '[ng-controller^="CustomPageController_AIPImmunizationSurvey"]';
-var context = $(contextSelector);
+(function($) {
+  $(function() {
+    var contextSelector = '[ng-controller^="CustomPageController_AIPImmunizationSurvey"]';
+    var context = $(contextSelector);
 
-window.__context = context;
-window.__ImmunizationRecord = $ImmunizationRecord;
+    var doUpdateImmunzationSurvey = function(event) {
+      var selectedRadio = $('.check-list input[type="radio"]:checked', context);
+      var commentBox = $('textarea', context);
 
-$hasImmunizationRecord = false;
+      var data = {
+        immunization_comment: commentBox.val(),
+        imst_code: selectedRadio.val(),
+        immu_code: 'COVID-19',
+      };
+      var queryParams = null;
 
-window.doUpdateImmunzationSurvey = (event) => {
-  var selectedRadio = $('.check-list input[type="radio"]:checked', context);
-  var commentBox = $('textarea', context);
-
-  var data = {
-    immunization_comment: commentBox.val(),
-    imst_code: selectedRadio.val(),
-  };
-  var queryParams = null;
-  console.log(data)
-
-  $ImmunizationRecord.post(
-    data,
-    queryParams,
-    function () {
-      alert("Immunization Survey submitted successfully.", {
-        type: "success",
-        flash: true,
-      });
-    },
-    function (response) {
-console.log(arguments, data, selectedRadio, commentBox)
-      alert(response.data.errors.errorMessage, {
-        type: "error",
-        flash: true,
-      });
-    }
-  );
-};
-
-$("#pbid-ActionItemContentDetail-save-button", context).attr(
-  "onclick",
-  "return doUpdateImmunzationSurvey(event);"
-);
-
-$('.check-list', context).on(
-  "change",
-  'input[type="radio"]',
-  function (event) {
-      var isChecked = $(".check-list input[type='radio']:checked", context).length === 1;
-
-      if(isChecked) {
-        if(!$hasImmunizationRecord) {
-            params.isResponseModified = true;
+      $ImmunizationRecord.post(
+        data,
+        queryParams,
+        function () {
+          alert("Immunization Survey submitted successfully.", {
+            type: "success",
+            flash: true,
+          });
+        },
+        function (response) {
+          alert(response.data.errors.errorMessage, {
+            type: "error",
+            flash: true,
+          });
         }
-        $("#pbid-ActionItemContentDetail-save-button", context).removeAttr(
-          "disabled"
-        );
-      } else {
-        params.isResponseModified = false;
-        $("#pbid-ActionItemContentDetail-save-button", context).attr("disabled", "disabled");
+      );
+      debugger
+      $ActionItemStatusDetail.$save();
+    };
+
+    $("#pbid-ActionItemContentDetail-save-button", context).on(
+      "click",
+      doUpdateImmunzationSurvey,
+    );
+
+    $('body').on(
+      "change",
+      `${contextSelector} .check-list input[type="radio"]`,
+      function (event) {
+          var isChecked = $(".check-list input[type='radio']:checked", context).length === 1;
+
+          if(isChecked) {
+            params.isResponseModified = true;
+            $("#pbid-ActionItemContentDetail-save-button", context).removeAttr(
+              "disabled"
+            );
+          } else {
+            params.isResponseModified = false;
+            $("#pbid-ActionItemContentDetail-save-button", context).attr("disabled", "disabled");
+          }
       }
-  }
-);
-setTimeout( () => {
-    //$ActionItemStatusAgree.load();
-    if($ActionItemContentDetail && $ActionItemContentDetail.STATUS_COUNT > 0) {
-        $hasImmunizationRecord = true;
-    }
-},
-300);
+    );
+  });
+})(jQuery);
