c6cdd117a865eef9399457c84674cfba98fb42a6
John Pope <75339810+john-pope@users.noreply.github.com>
2021-10-14 09:16:16 -0600

    add in ActionItemStatus model (radio) for AIP trigger to work

--- AIPImmunizationSurvey/ActionItemContactDetail_onload.js
+++ AIPImmunizationSurvey/ActionItemContactDetail_onload.js
@@ -6,8 +6,22 @@
     var doUpdateImmunzationSurvey = function(event) {
       var selectedRadio = $('.check-list input[type="radio"]:checked', context);
       var commentBox = $('textarea', context);
+      var aipRadio = $('#pbid-ActionItemStatusAgree-radio-0-0', context);
 
-      var data = {
+      // AIP state changes are updated on click...
+      // TODO: see if we can simplify this. Might be a $load or other state variable that we can set to ensure that a save happens
+      // This seems to work, but did have 403 errors at one point (may have just been session issues on zpprd)
+      // $scope.ActionItemContent.put({
+      //   "STATUS_RULE_ID": "32",  // params.actionItemId
+      //   "id": "3337", // not sure how to get this dynamically...
+      // })
+      // for now trigger a click, then make sure the state is set to checked
+      // with this method, the AIP logic only runs if it hasn't bee set yet, may be better to keep that functionality
+debugger
+      aipRadio.trigger('click');
+      aipRadio.prop('checked', true);
+
+      var immunizationRecordData = {
         immunization_comment: commentBox.val(),
         imst_code: selectedRadio.val(),
         immu_code: 'COVID-19',
@@ -15,7 +29,7 @@
       var queryParams = null;
 
       $ImmunizationRecord.post(
-        data,
+        immunizationRecordData ,
         queryParams,
         function () {
           alert("Immunization Survey submitted successfully.", {
@@ -30,8 +44,10 @@
           });
         }
       );
-      debugger
-      $ActionItemStatusDetail.$save();
+
+      params.isResponseModified = true;
+      // uses the ActionItemContentDetail, ActionItemStatus radio must be a child of this for AIP to update!
+      $ActionItemContentDetail.$save();
     };
 
     $("#pbid-ActionItemContentDetail-save-button", context).on(
