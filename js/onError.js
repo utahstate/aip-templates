0a5722a8fced1aeb77363170c36bdd28962a21c0
Carl Ellsworth <carl.ellsworth@usu.edu>
2022-05-13 11:30:28 -0600

    improve examples for onError javascript

--- js/onError.js
+++ js/onError.js
@@ -1,7 +1,14 @@
-//code for "onError"
+// ellucian recomended code for "onError"
 var logMessage = response.data.errors.errorMessage;
 alert(logMessage)
 
+// Example for alert after load error
+alert(response.data.errors[0].errorMessage,{type:"error"});
+
+// Example for alert after save error
+alert(response.data.errors.errorMessage,{type:"error"});
+
+// Combined example of "onError" used in many demos
 if (response.status == 404)
     alert('invalid resource');
 else if (response.status == 500) {
