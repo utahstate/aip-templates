3b42f2fce4574c61e8ddd6fdc21ec7d4ed8409a3
Autumn Canfield <autumn.a.canfield@gmail.com>
2024-10-10 13:04:49 -0600

    Add stuLicensureProgramResponse sql

--- AIPLicensurePrograms/stuLicensureProgramResponse_post.sql
+++ AIPLicensurePrograms/stuLicensureProgramResponse_post.sql
@@ -1,4 +1,6 @@
-INSERT INTO baninst1.ZSTULPR (ZSTULPR_PIDM,
-                              ZSTULPR_STATE_CODE,
-                              ZSTULPR_ACKNOWLEDGE_DATE)
-     VALUES ( :parm_user_pidm, :STATE_CODE, SYSDATE);
+BEGIN
+  INSERT INTO baninst1.ZSTULPR (ZSTULPR_PIDM,
+                                ZSTULPR_STATE_CODE,
+                                ZSTULPR_AKNOWLEDGE_DATE)
+  VALUES ( :parm_user_pidm, :STATE_CODE, SYSDATE);
+END;
